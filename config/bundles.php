74b7775f00c351beaed342f0aeb5d1ff559010fc
BENNACER Mohamed Akram <km_bennacer@esi.dz>
2023-03-24 11:29:36 +0100

    mailer pending issue solved

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
